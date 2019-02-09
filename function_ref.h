f9d76cc4ba8df8beb1bfbca8507891e5a83dab55
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2019-02-09 16:58:02 +0000

    Working passing member functions

--- function_ref.h
+++ function_ref.h
@@ -1,8 +1,8 @@
 #pragma once
 
+#include <type_traits>
+#include <utility>
 #include <exception>
-
-namespace tools {
 
 template <typename R, typename... Args>
 class function_ref;
@@ -13,34 +13,55 @@ class function_ref<R(Args...)> {
 
   union erased_data {
     no_first_parametr_ptr_t function_without_first_parametr;
-    void* data_;
+    void* ptr;
   };
-  using erased_fn_t = R (*)(const function_ref&, Args...);
+  using erased_fn_t = R (*)(erased_data, Args...);
 
   // Type erasure tags
-  static constexpr inline erased_fn_t null = [](const function_ref&, Args...) -> R {
-    throw std::runtime_error("attemtp to call empty function_ref");  // Probably want to help non-throwing
-                                                                     // envirenmbents here.
+  static constexpr inline erased_fn_t null = [](erased_data, Args...) -> R {
+    throw std::runtime_error(
+        "attemtp to call empty function_ref");  // Probably want to help
+                                                // non-throwing envirenmbents
+                                                // here.
+  };
+
+  static constexpr inline erased_fn_t plain_function = [](erased_data data, Args... args) -> R {
+    return data.function_without_first_parametr(args...);
   };
 
   erased_fn_t indirect_ = null;
   erased_data data_;
 
  public:
+  constexpr function_ref() = default;
+  constexpr function_ref(no_first_parametr_ptr_t func)
+      : indirect_(plain_function) {
+    data_.function_without_first_parametr = func;
+  }
 
-   function_ref() = default;
-   function_ref(const function_ref&) noexcept = default;
-   function_ref(function_ref&&) noexcept = default;
+  template <typename T>
+  /*constexpr*/ function_ref(R(T::*method)(Args...), std::remove_reference_t<T>& object) {  // object should match qualifiers
+    // an intrinsic can do this.
+    struct split_memptr {
+        erased_fn_t fn;
+        std::ptrdiff_t offset;
+    };
 
-   function_ref& operator=(const function_ref&) noexcept = default;
-   function_ref& operator=(function_ref&&) noexcept = default;
+    // This should be an intensic:
+    split_memptr split = *reinterpret_cast<split_memptr*>(&method);
+    if (split.offset) {
+        throw std::runtime_error("Compiler should have removed the offset");
+    }
 
-   ~function_ref() noexcept = default;
+    indirect_ = split.fn;
+    data_.ptr = reinterpret_cast<void*>(&object);
+    /*unsigned char* as_raw_bytes = reinterpret_cast<unsigned char*>(&object);
+    as_raw_bytes += split.offset;
+    data_.ptr = as_raw_bytes;
+    */
+  }
 
-   R operator()(Args ... args) const {
-       return indirect_(*this, std::forward<Args>(args)...);
-   }
-
+  constexpr R operator()(Args... args) const {
+    return indirect_(data_, std::forward<Args>(args)...);
+  }
 };
-
-}  // namespace tools
--- function_ref_test.cc
+++ function_ref_test.cc
@@ -1,23 +1,66 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
+#include <cinttypes>
+#include <iostream>
+
 #include "function_ref.h"
 
 namespace {
 
 struct no_default {
-  no_default(int) {}
+  no_default(int x) : data{x} {}
+
+  int data;
 };
+
+no_default plain_function(int x) { return no_default{x}; }
+
+struct static_function {
+  static no_default function(int x) { return no_default(x); }
+};
+
 
 }  // namespace
 
 namespace tools {
 
 TEST_CASE("function_ref, empty") {
-    function_ref<no_default(int)> f;
-    try {
-      f(3);
-    } catch(const std::runtime_error&) {}
+  function_ref<no_default(int)> f;
+  try {
+    f(3);
+  } catch (const std::runtime_error&) {
+  }
+}
+
+TEST_CASE("function_ref, function") {
+  function_ref<no_default(int)> f{plain_function};
+
+  CHECK(f(5).data == 5);
+
+  f = static_function::function;
+  CHECK(f(4).data == 4);
+}
+
+TEST_CASE("funciton_ref, member + this no virtual") {
+  struct base_class_A {
+    int dataA = 1;
+    no_default functionA(int x) { return no_default(dataA * x); }
+  };
+
+  struct base_class_B {
+    int dataB = 2;
+    no_default functionB(int x) { return no_default(dataB * 3); }
+  };
+
+  struct multiple_baseclasses : base_class_A, base_class_B {};
+
+  multiple_baseclasses object;
+
+  function_ref<no_default(int)> f{&multiple_baseclasses::functionA, object};
+  CHECK(f(3).data == 3);
+  f = function_ref<no_default(int)>{&multiple_baseclasses::functionB, object};
+  CHECK(f(3).data == 6);
 }
 
 }  // namespace tools
